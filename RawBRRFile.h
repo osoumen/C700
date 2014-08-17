d60a020a0d12da8cff0a3f7b249c93e2e003bf97
osoumen <osoumen@picopicose.com>
2014-08-17 11:16:11 +0900

    ピッチベンドレンジをMIDIch毎に独立。（RPNで設定する必要あり） adsrパラメータをコントロールチェンジで一時的に変更出来るようにした。

--- RawBRRFile.h
+++ RawBRRFile.h
@@ -13,23 +13,6 @@
 
 class RawBRRFile : public FileAccess {
 public:
-	static const int	HAS_PGNAME = 1 << 0;
-	static const int	HAS_RATE = 1 << 1;
-	static const int	HAS_BASEKEY = 1 << 2;
-	static const int	HAS_LOWKEY = 1 << 3;
-	static const int	HAS_HIGHKEY = 1 << 4;
-	static const int	HAS_AR = 1 << 5;
-	static const int	HAS_DR = 1 << 6;
-	static const int	HAS_SL = 1 << 7;
-	static const int	HAS_SR = 1 << 8;
-	static const int	HAS_VOLL = 1 << 9;
-	static const int	HAS_VOLR = 1 << 10;
-	static const int	HAS_ECHO = 1 << 11;
-	static const int	HAS_BANK = 1 << 12;
-	static const int	HAS_ISEMPHASIZED = 1 << 13;
-	static const int	HAS_SOURCEFILE = 1 << 14;
-    static const int    HAS_SUSTAINMODE = 1 << 15;
-	
 	RawBRRFile( const char *path, bool isWriteable=true );
 	virtual ~RawBRRFile();
 	
