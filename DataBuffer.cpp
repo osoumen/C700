64b6534136cafed930457fd28cfe15d8bc9c6ee1
osoumen <osoumen@picopicose.com>
2017-03-18 14:24:47 +0900

    Windows環境向けのcreateParentfolder()の実装

--- DataBuffer.cpp
+++ DataBuffer.cpp
@@ -11,9 +11,10 @@
 #include <string.h>
 #if MAC
 #include <sys/stat.h>
+#endif
 void createParentfolder(const char *path);
 void getFileNameParentPath(const char *path, char *out, int maxLen);
-#endif
+
 
 //-----------------------------------------------------------------------------
 DataBuffer::DataBuffer(int allocMemSize)
@@ -265,10 +266,10 @@ bool DataBuffer::WriteToFile(const char *path)
 {
     // Buffer にあるデータを指定したファイルパスに書き込む
     
+	// 親フォルダが存在しなければ作成する
+	createParentfolder(path);
+
 #if MAC
-    // 親フォルダが存在しなければ作成する
-    createParentfolder(path);
-    
     CFURLRef	savefile = CFURLCreateFromFileSystemRepresentation(NULL, (UInt8*)path, strlen(path), false);
     
     CFWriteStreamRef	filestream = CFWriteStreamCreateWithFile(NULL,savefile);
@@ -292,24 +293,43 @@ bool DataBuffer::WriteToFile(const char *path)
 }
 
 //-----------------------------------------------------------------------------
-#if MAC
+#if WIN32
 void createParentfolder(const char *path)
 {
-    char directory[PATH_LEN_MAX];
-    
-    getFileNameParentPath(path, directory, PATH_LEN_MAX);
-    
-    struct stat m;
-    stat(directory, &m);
-    if (S_ISDIR(m.st_mode)) {
-        // 親フォルダは存在する
-        return;
-    }
-    if (strncmp(path, directory, PATH_LEN_MAX) == 0) {
-        return;
-    }
-    createParentfolder(directory);
-    mkdir(directory, 0777);
+	char directory[PATH_LEN_MAX];
+
+	getFileNameParentPath(path, directory, PATH_LEN_MAX);
+
+	WIN32_FIND_DATA stat;
+	HANDLE handle = FindFirstFile(directory, &stat);
+	if (stat.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+		// 親フォルダは存在する
+		return;
+	}
+	if (strncmp(path, directory, PATH_LEN_MAX) == 0) {
+		return;
+	}
+	createParentfolder(directory);
+	CreateDirectory(directory, NULL);
+}
+#else
+void createParentfolder(const char *path)
+{
+	char directory[PATH_LEN_MAX];
+
+	getFileNameParentPath(path, directory, PATH_LEN_MAX);
+
+	struct stat m;
+	stat(directory, &m);
+	if (S_ISDIR(m.st_mode)) {
+		// 親フォルダは存在する
+		return;
+	}
+	if (strncmp(path, directory, PATH_LEN_MAX) == 0) {
+		return;
+	}
+	createParentfolder(directory);
+	mkdir(directory, 0777);
 }
 #endif
 
