e5219e75fd077c4e134f72b68ae09b72f1bbf010
osoumen <osoumen@picopicose.com>
2021-10-23 16:41:53 +0900

    windowsでビルド出来ない箇所を修正

--- commusb/ControlUSB.cpp
+++ commusb/ControlUSB.cpp
@@ -713,14 +713,14 @@ bool ControlUSB::openDevice(std::basic_string<TCHAR> devpath)
 		NULL);
 
 	if (hNewDev == INVALID_HANDLE_VALUE) {
-		goto bail;
+		return false;
 	}
 
 	HANDLE hNewWinUsb = NULL;
 	if (!WinUsb_Initialize(hNewDev, &hNewWinUsb)) {
 		//DWORD err = GetLastError();
 		CloseHandle(hNewDev);
-		goto bail;
+		return false;
 	}
 
 	// エンドポイント情報取得
@@ -728,7 +728,7 @@ bool ControlUSB::openDevice(std::basic_string<TCHAR> devpath)
 	if (!WinUsb_QueryInterfaceSettings(hNewWinUsb, 0, &desc)) {
 		WinUsb_Free(hNewWinUsb);
 		CloseHandle(hNewDev);
-		goto bail;
+		return false;
 	}
 
 	for (int i = 0; i<desc.bNumEndpoints; i++) {
